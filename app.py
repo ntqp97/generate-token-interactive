f45ecfc24b747dba3ef9e6bbac0bd4eeb42b4947
Nguyen The Quang Phuoc <phuoc.nguyenthequang@teragroup.io>
2023-05-22 15:23:57 +0700

    update code

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 def hello_world():
     list_token = []
     data = request.get_json()
-    for profile in data:
+    for profile in json.loads(data['profiles']):
         private_key = os.environ.get('private_key')
-        token = jwt.encode(json.loads(profile), private_key, algorithm='ES256', )
+        token = jwt.encode(profile, private_key, algorithm='ES256', )
         list_token.append(token)
     return {'token': list_token}
