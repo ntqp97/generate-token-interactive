f9ec8289654477d027a820bc8bcf632de5d0137f
Nguyen The Quang Phuoc <phuoc.nguyenthequang@teragroup.io>
2023-05-22 15:11:09 +0700

    update code

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/generate-token-to-interactive', methods=['POST'])
 def hello_world():
+    list_token = []
     data = request.get_json()
-    private_key = os.environ.get('private_key')
-    token = jwt.encode(data, private_key, algorithm='ES256', )
-    return {'token': token}
+    for profile in data:
+        private_key = os.environ.get('private_key')
+        token = jwt.encode(profile, private_key, algorithm='ES256', )
+        list_token.append(token)
+    return {'token': list_token}
