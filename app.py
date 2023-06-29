3fde27ae782c2ba872805efe13c4d72e2074361b
Nguyen The Quang Phuoc <phuoc.nguyenthequang@teragroup.io>
2023-06-29 09:52:49 +0700

    update code

--- app.py
+++ app.py
@@ -9,7 +9,13 @@ def hello_world():
     list_token = []
     data = request.get_json()
     for profile in data['profiles']:
-        private_key = os.environ.get('private_key')
-        token = jwt.encode(profile, private_key, algorithm='ES256', )
+        env = data.get('env', 'dev')
+        if env == 'dev':
+            private_key = os.environ.get('private_key_dev')
+        if env == 'stg':
+            private_key = os.environ.get('private_key_stg')
+        if env == 'prd':
+            private_key = os.environ.get('private_key_prd')
+        token = jwt.encode(profile, private_key, algorithm='RS256', )
         list_token.append(token)
     return {'token': list_token}
